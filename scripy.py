c2829c2a780172c3da7354f7ab36a4a6701f7d7b
Ritwik Jha <59885389+ritwik-jha@users.noreply.github.com>
2021-01-02 15:44:52 +0530

    Update scripy.py

--- scripy.py
+++ scripy.py
@@ -21,27 +21,27 @@ x = int(x)
 
 if x==1:
 	y = input('Enter the partition name: ')
-  	os.system('pvcreate {}'.format(y))
+	os.system('pvcreate {}'.format(y))
 elif x==2:
-  	y = input('Enter name of the pv: ')
-  	os.system('pvdisplay {}'.format(y))
+	y = input('Enter name of the pv: ')
+	os.system('pvdisplay {}'.format(y))
 elif x==3:
-  	y = input('Enter name of volume group(vg) : ')
-  	z = input("Enter name of the pv's which wil contribute the storage to vg (in continous way) : ")
-  	os.system('vgcreate {} {}'.format(y,z))
+	y = input('Enter name of volume group(vg) : ')
+	z = input("Enter name of the pv's which wil contribute the storage to vg (in continous way) : ")
+	os.system('vgcreate {} {}'.format(y,z))
 elif x==4:
-  	y = input('Enter the name of vg : ')
-  	os.system('vgdisplay {}'.format(y))
+	y = input('Enter the name of vg : ')
+	os.system('vgdisplay {}'.format(y))
 elif x ==5:
-  	y = input('Enter name of vg: ')
-  	z = input('Enter name of lv: ')
-  	a = input('Enter size of lv: ')
-  	os.system('lvcreate --size {}  --name {}  {}'.format(a,z,y))
+	y = input('Enter name of vg: ')
+	z = input('Enter name of lv: ')
+	a = input('Enter size of lv: ')
+	os.system('lvcreate --size {}  --name {}  {}'.format(a,z,y))
 	os.system('mkfs.ext4 /dev/{}/{}'.format(y,z))
 elif x==6:
-  	y = input('Enter name of vg: ')
-  	z = input('Enter name of lv: ')
-  	os.system('lvdisplay {}/{}'.format(y,z))
+	y = input('Enter name of vg: ')
+	z = input('Enter name of lv: ')
+	os.system('lvdisplay {}/{}'.format(y,z))
 elif x == 7:
 	y = input('Enter name of lv (as vg_name/lv_name): ')
 	z = input('Enter the size and units to be extended (as size {G,M,T}): ')
