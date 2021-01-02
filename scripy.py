932cbc378e5cef9311e40fbd1138e6330da468f1
Ritwik Jha <59885389+ritwik-jha@users.noreply.github.com>
2021-01-02 15:18:51 +0530

    Update scripy.py

--- scripy.py
+++ scripy.py
@@ -13,14 +13,14 @@ print(""""
       4. Display the details of VG.
       5. Create and format a Logical Volume (LV).
       6. Show details of the LV.
-			7. Increase the size of LV.
+      7. Increase the size of LV.
       """)
 
 x = input('What do you want to do (Enter the number)')
 x = int(x)
 
 if x==1:
-		y = input('Enter the partition name: ')
+	y = input('Enter the partition name: ')
   	os.system('pvcreate {}'.format(y))
 elif x==2:
   	y = input('Enter name of the pv: ')
@@ -37,15 +37,15 @@ elif x ==5:
   	z = input('Enter name of lv: ')
   	a = input('Enter size of lv: ')
   	os.system('lvcreate --size {}  --name {}  {}'.format(a,z,y))
-		os.system('mkfs.ext4 /dev/{}/{}'.format(y,z))
+	os.system('mkfs.ext4 /dev/{}/{}'.format(y,z))
 elif x==6:
   	y = input('Enter name of vg: ')
   	z = input('Enter name of lv: ')
   	os.system('lvdisplay {}/{}'.format(y,z))
 elif x == 7:
-		y = input('Enter name of lv (as vg_name/lv_name): ')
-		z = input('Enter the size and units to be extended (as size {G,M,T}): ')
-		os.system('lvextend --size +{}  {}'.format(z,y))
-		os.system('resize2fs /dev/{}'.format(y))
+	y = input('Enter name of lv (as vg_name/lv_name): ')
+	z = input('Enter the size and units to be extended (as size {G,M,T}): ')
+	os.system('lvextend --size +{}  {}'.format(z,y))
+	os.system('resize2fs /dev/{}'.format(y))
 else:
-		print('no matches found')
+	print('no matches found')
